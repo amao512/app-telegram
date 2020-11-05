d21a5ed73236f792143b28e67336b88ea1831f3b
aslnstbk <asyl.zed.han@gmail.com>
2020-11-05 13:56:06 +0600

    add: add settings fragment elements

--- app/src/main/java/com/aslnstbk/telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/aslnstbk/telegram/ui/fragments/SettingsFragment.kt
@@ -1,10 +1,30 @@
 package com.aslnstbk.telegram.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import com.aslnstbk.telegram.R
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.settings_action_menu_edit -> Toast.makeText(activity, "Edit", Toast.LENGTH_SHORT).show()
+            R.id.settings_action_menu_exit -> Toast.makeText(activity, "Exit", Toast.LENGTH_SHORT).show()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
